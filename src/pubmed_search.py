01e7d6a0c8b2134988165a780b305a92b4fde9a6
Winston T. Chu <wtchu8@gmail.com>
2023-05-12 09:03:08 -0400

    backup pubmed search

---
+++ src/pubmed_search.py
@@ -0,0 +1,37 @@
+(
+    (
+        (
+            (
+                (machine learning) OR (deep learning)
+            ) OR (artificial intelligence)
+        ) 
+        AND 
+        (
+            (
+                (
+                    (
+                        (
+                            (
+                                (
+                                    (medical imaging) OR (computed tomography)
+                                ) OR (magnetic resonance imaging)
+                            ) OR (ultrasound)
+                        ) OR (PET)
+                    ) OR (SPECT)
+                ) OR (CT)
+            ) OR (MRI)
+        )
+    )
+    AND 
+    (
+        (
+            (
+                (
+                    (
+                        (infectious) OR (EBOV)
+                    ) OR (SARS-CoV-2)
+                ) OR (LASSA)
+            ) OR (Nipah)
+        ) OR (SHFV)
+    )
+)
